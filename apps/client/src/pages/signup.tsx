15d63fc835060617237fa8c2fc0216998055fdfa
Sunil <sunisimar2002d@gmail.com>
2023-12-09 00:01:33 +0530

    adding nextjs

---
+++ apps/client/src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import { Signup } from "@repo/ui";
+
+export default function SignupPage() {
+  return (
+    <div>
+      <Signup></Signup>
+    </div>
+  );
+}
---
+++ packages/ui/components/Signup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Signup() {
+  return (
+    <div>
+      <input type="text" placeholder="email"></input>
+      <input type="text" placeholder="password"></input>
+      <button>Submit</button>
+    </div>
+  );
+}
