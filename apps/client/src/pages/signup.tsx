104cdb4d7631eeb5391be47cdc3595d7272055b1
Sunil <sunisimar2002d@gmail.com>
2023-12-09 01:01:22 +0530

    adding singupPage

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -3,7 +3,14 @@ import { Signup } from "@repo/ui";
 export default function SignupPage() {
   return (
     <div>
-      <Signup></Signup>
+      <Signup
+        onClick={async (username, password) => {
+          // const response = await axios.post("",{
+          //     username,
+          //     password
+          // })
+        }}
+      />
     </div>
   );
 }
--- packages/ui/components/Signup.tsx
+++ packages/ui/components/Signup.tsx
@@ -1,11 +1,62 @@
-import React from "react";
+import { Button, Card, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
-export function Signup() {
+export function Signup(props: {
+  onClick: (username: string, password: string) => void;
+}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
-      <input type="text" placeholder="email"></input>
-      <input type="text" placeholder="password"></input>
-      <button>Submit</button>
+      <div
+        style={{
+          paddingTop: 150,
+          marginBottom: 10,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant={"h6"}>
+          Welcome to Coursera. Sign up below
+        </Typography>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card style={{ width: 400, padding: 20 }}>
+          <TextField
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+          />
+          <br />
+          <br />
+          <TextField
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            fullWidth={true}
+            label="Password"
+            variant="outlined"
+            type={"password"}
+          />
+          <br />
+          <br />
+
+          <Button
+            size={"large"}
+            variant="contained"
+            onClick={async () => {
+              props.onClick(email, password);
+            }}
+          >
+            {" "}
+            Signup
+          </Button>
+        </Card>
+      </div>
     </div>
   );
 }
