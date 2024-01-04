0a419aa9cfb80a1781fd83fc021619e1bfd8388e
Sunil <sunisimar2002d@gmail.com>
2024-01-04 23:01:27 +0530

    adding recoil

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@repo/ui", "db"],
+  transpilePackages: ["@repo/ui", "db", "store"],
 };
 
 module.exports = nextConfig;
---
+++ packages/store/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./atoms/course";
+export * from "./atoms/user";
+export * from "./selectors/userEmail";
+export * from "./selectors/userLoading";
